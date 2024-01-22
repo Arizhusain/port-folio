7d1f1f8c470b501827508773910e244cb0110cb9
Arizhusain <arifhusainsoudagar01@gmail.com>
2024-01-22 19:11:44 +0530

    final design

--- src/Components/features/Card.jsx
+++ src/Components/features/Card.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import './feature.css'
+import React from "react";
+import "./feature.css";
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div>
-      Card
-    </div>
-  )
-}
+    <>
+      <div class="smallcard">
+        <div class="smallbg">
+          <div className="smallImage">Image</div>
+          <h1 className="smallTitle">UI Design</h1>
+          <p className="smallDesc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+            quod?
+          </p>
+          <a href="#" className="smallLink">
+            KNOW MORE...
+          </a>
+        </div>
+        <div class="smallblob"></div>
+      </div>
+    </>
+  );
+};
 
-export default Card
+export default Card;
--- src/Components/features/Rating.jsx
+++ src/Components/features/Rating.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./feature.css";
 
 const Rating = () => {
-  return <div>Rating</div>;
+  return (
+    <div class="card">
+      <p class="heading">Popular this month</p>
+      <p>60+</p>
+      <p>Uiverse</p>
+    </div>
+  );
 };
 
 export default Rating;
